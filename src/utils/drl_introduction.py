ca1184959205f0e1965e2355b364879ed61e6001
N. L. <aeternalis-ingenium@proton.me>
2024-11-16 20:20:55 +0100

    feat(chapter-1-and-2): finishing up chapter 1 and 2

--- src/utils/drl_introduction.py
+++ src/utils/drl_introduction.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def plot_blackjack_values(V):
-
     def get_Z(x, y, usable_ace):
         if (x,y,usable_ace) in V:
             return V[x,y,usable_ace]
@@ -19,22 +18,21 @@ def plot_blackjack_values(V):
         Z = np.array([get_Z(x,y,usable_ace) for x,y in zip(np.ravel(X), np.ravel(Y))]).reshape(X.shape)
 
         surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.coolwarm, vmin=-1.0, vmax=1.0)
-        ax.set_xlabel('Player\'s Current Sum')
-        ax.set_ylabel('Dealer\'s Showing Card')
-        ax.set_zlabel('State Value')
+        ax.set_xlabel("Player\'s Current Sum")
+        ax.set_ylabel("Dealer\'s Showing Card")
+        ax.set_zlabel("State Value")
         ax.view_init(ax.elev, -120)
 
     fig = plt.figure(figsize=(20, 20))
-    ax = fig.add_subplot(211, projection='3d')
-    ax.set_title('Usable Ace')
+    ax = fig.add_subplot(211, projection="3d")
+    ax.set_title("Usable Ace")
     get_figure(True, ax)
-    ax = fig.add_subplot(212, projection='3d')
-    ax.set_title('No Usable Ace')
+    ax = fig.add_subplot(212, projection="3d")
+    ax.set_title("No Usable Ace")
     get_figure(False, ax)
     plt.show()
 
 def plot_policy(policy):
-
     def get_Z(x, y, usable_ace):
         if (x,y,usable_ace) in policy:
             return policy[x,y,usable_ace]
@@ -46,23 +44,23 @@ def plot_policy(policy):
         y_range = np.arange(10, 0, -1)
         X, Y = np.meshgrid(x_range, y_range)
         Z = np.array([[get_Z(x,y,usable_ace) for x in x_range] for y in y_range])
-        surf = ax.imshow(Z, cmap=plt.get_cmap('Pastel2', 2), vmin=0, vmax=1, extent=[10.5, 21.5, 0.5, 10.5])
+        surf = ax.imshow(Z, cmap=plt.get_cmap("Pastel2", 2), vmin=0, vmax=1, extent=[10.5, 21.5, 0.5, 10.5])
         plt.xticks(x_range)
         plt.yticks(y_range)
         plt.gca().invert_yaxis()
-        ax.set_xlabel('Player\'s Current Sum')
-        ax.set_ylabel('Dealer\'s Showing Card')
-        ax.grid(color='w', linestyle='-', linewidth=1)
+        ax.set_xlabel("Player\'s Current Sum")
+        ax.set_ylabel("Dealer\'s Showing Card")
+        ax.grid(color="w", linestyle="-", linewidth=1)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.1)
         cbar = plt.colorbar(surf, ticks=[0,1], cax=cax)
-        cbar.ax.set_yticklabels(['0 (STICK)','1 (HIT)'])
+        cbar.ax.set_yticklabels(["0 (STICK)","1 (HIT)"])
             
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(121)
-    ax.set_title('Usable Ace')
+    ax.set_title("Usable Ace")
     get_figure(True, ax)
     ax = fig.add_subplot(122)
-    ax.set_title('No Usable Ace')
+    ax.set_title("No Usable Ace")
     get_figure(False, ax)
     plt.show()
